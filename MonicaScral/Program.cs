5e1a38fd9eaef77b5e9360ee3947eb493162fbb9
BlubbFish <git@blubbfish.net>
2019-04-17 16:07:23 +0200

    Init

---
+++ MonicaScral/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using BlubbFish.Utils;
+
+namespace Fraunhofer.Fit.IoT.MonicaScral {
+  class Program {
+    private Thread sig_thread;
+    private Boolean RunningProcess = true;
+    protected ProgramLogger logger = new ProgramLogger();
+
+    static void Main(String[] args) => new Program(args);
+
+    public Program(String[] args) {
+      MqttListener m = new MqttListener(new Dictionary<String, String>() { { "type", "mqtt" }, { "server", "10.100.0.20" }, { "topic", "lora/data/+;lora/panic/+" } });
+      ScralPusher s = new ScralPusher(new Dictionary<String, String>() {
+        { "server", "http://monappdwp3.monica-cloud.eu:8250" },
+        { "register_addr", "/scral/v1.0/gps-tracker-gw/gps-tag" },
+        { "register_method", "post" },
+        { "update_addr", "/scral/v1.0/gps-tracker-gw/gps-tag/localization" },
+        { "update_method", "put" },
+        { "panic_addr", "/scral/v1.0/gps-tracker-gw/gps-tag/alert" },
+        { "panic_method", "put" },});
+      m.Update += s.DataInput;
+      this.WaitForShutdown();
+      m.Dispose();
+      s.Dispose();
+    }
+
+    protected void WaitForShutdown() {
+      if(Type.GetType("Mono.Runtime") != null) {
+        this.sig_thread = new Thread(delegate () {
+          Mono.Unix.UnixSignal[] signals = new Mono.Unix.UnixSignal[] {
+            new Mono.Unix.UnixSignal(Mono.Unix.Native.Signum.SIGTERM),
+            new Mono.Unix.UnixSignal(Mono.Unix.Native.Signum.SIGINT)
+          };
+          Console.WriteLine("BlubbFish.Utils.IoT.Bots.Bot.WaitForShutdown: Signalhandler Mono attached.");
+          while(true) {
+            Int32 i = Mono.Unix.UnixSignal.WaitAny(signals, -1);
+            Console.WriteLine("BlubbFish.Utils.IoT.Bots.Bot.WaitForShutdown: Signalhandler Mono INT recieved " + i + ".");
+            this.RunningProcess = false;
+            break;
+          }
+        });
+        this.sig_thread.Start();
+      } else {
+        Console.CancelKeyPress += new ConsoleCancelEventHandler(this.SetupShutdown);
+        Console.WriteLine("BlubbFish.Utils.IoT.Bots.Bot.WaitForShutdown: Signalhandler Windows attached.");
+      }
+      while(this.RunningProcess) {
+        Thread.Sleep(100);
+      }
+    }
+
+    private void SetupShutdown(Object sender, ConsoleCancelEventArgs e) {
+      e.Cancel = true;
+      Console.WriteLine("BlubbFish.Utils.IoT.Bots.Bot.SetupShutdown: Signalhandler Windows INT recieved.");
+      this.RunningProcess = false;
+    }
+  }
+}
---
+++ MonicaScral/ScralPusher.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using BlubbFish.Utils;
+using LitJson;
+
+namespace Fraunhofer.Fit.IoT.MonicaScral {
+  class ScralPusher {
+    private readonly List<String> nodes = new List<String>();
+    private readonly Dictionary<String, String> config;
+    private readonly Object getLock = new Object();
+    private readonly Boolean authRequired = false;
+    private readonly String auth = "";
+
+    public ScralPusher(Dictionary<String, String> settings) => this.config = settings;
+
+    internal async void DataInput(Object sender, MqttEvents e) => await Task.Run(() => {
+      try {
+        JsonData data = JsonMapper.ToObject(e.Message);
+        if(this.CheckRegister(data)) {
+          if(e.Topic.StartsWith("lora/data")) {
+            this.SendUpdate(data);
+          } else if(e.Topic.StartsWith("lora/panic")) {
+            this.SendPanic(data);
+          }
+        }
+      } catch(Exception ex) {
+        Helper.WriteError("Something is wrong: " + ex.Message);
+      }
+    });
+
+    private Boolean CheckRegister(JsonData data) {
+      if(data.ContainsKey("Name") && data["Name"].IsString) {
+        if(!this.nodes.Contains((String)data["Name"])) {
+          this.SendRegister(data);
+          this.nodes.Add((String)data["Name"]);
+        }
+        return true;
+      }
+      return false;
+    }
+
+    private void SendRegister(JsonData data) {
+      Dictionary<String, Object> d = new Dictionary<String, Object> {
+        { "device", "wearable" },
+        { "sensor", "tag" },
+        { "type", "uwb" },
+        { "tagId", (String)data["Name"] },
+        { "timestamp", DateTime.Now.ToString("o") },
+        { "unitOfMeasurements", "meters" },
+        { "observationType", "propietary" },
+        { "state", "active" }
+      };
+      try {
+        String addr = this.config["register_addr"];
+        if(Enum.TryParse(this.config["register_method"], true, out RequestMethod meth)) {
+          this.RequestString(addr, JsonMapper.ToJson(d), false, meth);
+          Console.WriteLine(meth.ToString() + " " + this.config["register_addr"] + ": " + JsonMapper.ToJson(d));
+        }
+      } catch(Exception e) {
+        Helper.WriteError("Fraunhofer.Fit.IoT.MonicaScral.SendRegister: " + e.Message);
+      }
+    }
+
+    private void SendUpdate(JsonData data) {
+      if((Boolean)data["Gps"]["Fix"]) {
+        Dictionary<String, Object> d = new Dictionary<String, Object> {
+        { "type", "uwb" },
+        { "tagId", (String)data["Name"] },
+        { "timestamp", DateTime.Now.ToString("o") },
+        { "lat", (Double)data["Gps"]["Latitude"] },
+        { "lon", (Double)data["Gps"]["Longitude"] },
+        { "height", (Double)data["Gps"]["Height"] },
+        { "hdop", (Double)data["Gps"]["Hdop"] },
+        { "snr", (Double)data["Snr"] },
+        { "battery_level", (Double)data["BatteryLevel"] },
+        { "host", (String)data["Host"]}
+      };
+        try {
+          String addr = this.config["update_addr"];
+          if(Enum.TryParse(this.config["update_method"], true, out RequestMethod meth)) {
+            this.RequestString(addr, JsonMapper.ToJson(d), false, meth);
+            Console.WriteLine(meth.ToString() + " " + this.config["update_addr"] + ": " + JsonMapper.ToJson(d));
+          }
+        } catch(Exception e) {
+          Helper.WriteError("Fraunhofer.Fit.IoT.MonicaScral.SendUpdate: " + e.Message);
+        }
+      }
+    }
+
+    private void SendPanic(JsonData data) {
+      Dictionary<String, Object> d = new Dictionary<String, Object> {
+        { "type", "uwb" },
+        { "tagId", (String)data["Name"] },
+        { "timestamp", DateTime.Now.ToString("o") },
+        { "last_known_lat", (Double)data["Gps"]["LastLatitude"] },
+        { "last_known_lon", (Double)data["Gps"]["LastLongitude"] },
+        { "last_known_gps", DateTime.Parse((String)data["Gps"]["LastGPSPos"], DateTimeFormatInfo.InvariantInfo, DateTimeStyles.AssumeUniversal).ToLocalTime().ToString("o") }
+      };
+      try {
+        String addr = this.config["panic_addr"];
+        if(Enum.TryParse(this.config["panic_method"], true, out RequestMethod meth)) {
+          this.RequestString(addr, JsonMapper.ToJson(d), false, meth);
+          Console.WriteLine(meth.ToString() + " " + this.config["panic_addr"] + ": " + JsonMapper.ToJson(d));
+        }
+      } catch(Exception e) {
+        Helper.WriteError("Fraunhofer.Fit.IoT.MonicaScral.SendRegister: " + e.Message);
+      }
+    }
+
+    #region HTTP Request
+    private String RequestString(String address, String json = "", Boolean withoutput = true, RequestMethod method = RequestMethod.GET) {
+      String ret = null;
+      lock(this.getLock) {
+        HttpWebRequest request = WebRequest.CreateHttp(this.config["server"] + address);
+        request.Timeout = 2000;
+        if(this.authRequired) {
+          request.Headers.Add(HttpRequestHeader.Authorization, this.auth);
+        }
+        if(method == RequestMethod.POST || method == RequestMethod.PUT) {
+          Byte[] requestdata = Encoding.ASCII.GetBytes(json);
+          request.ContentLength = requestdata.Length;
+          request.Method = method.ToString();
+          request.ContentType = "application/json";
+          using(Stream stream = request.GetRequestStream()) {
+            stream.Write(requestdata, 0, requestdata.Length);
+          }
+        }
+        try {
+          using(HttpWebResponse response = (HttpWebResponse)request.GetResponse()) {
+            if(response.StatusCode == HttpStatusCode.Unauthorized) {
+              Console.Error.WriteLine("Benutzer oder Passwort falsch!");
+              throw new Exception("Benutzer oder Passwort falsch!");
+            }
+            if(withoutput) {
+              StreamReader reader = new StreamReader(response.GetResponseStream());
+              ret = reader.ReadToEnd();
+            }
+          }
+        } catch(Exception e) {
+          throw new WebException("Error while uploading to Scal. Resource: \"" + this.config["server"] + address + "\" Method: " + method + " Data: " + json + " Fehler: " + e.Message);
+        }
+      }
+      return ret;
+    }
+    private enum RequestMethod {
+      GET,
+      POST,
+      PUT
+    }
+    #endregion
+
+    internal void Dispose() { }
+  }
+}
